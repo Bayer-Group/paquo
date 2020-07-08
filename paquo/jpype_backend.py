0d9c10c5b90f70b0f85d432242247867e1f070ed
Santi Villalba <sdvillal@gmail.com>
2020-07-08 13:59:22 +0200

    initial jpype setup function

---
+++ paquo/jpype_backend.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+import jpype
+
+from paquo.qupath import find_qupath
+
+
+def start_jpype():
+    if not jpype.isJVMStarted():
+        # For the time being, we assume qupath is our JVM of choice
+        app_dir, runtime_dir, jvm_path, jvm_options = find_qupath()
+        # This is not really needed, but beware we might need SL4J classes (see warning)
+        jpype.addClassPath(str(app_dir / '*'))
+        # Our custom java libraries
+        JAVA_VENDORED_PATH = Path(__file__).absolute().parent / 'vendored' / 'java' / 'jars'
+        if JAVA_VENDORED_PATH.is_dir():
+            jpype.addClassPath(str(JAVA_VENDORED_PATH / '*'))
+        jpype.startJVM(str(jvm_path), convertStrings=False)
