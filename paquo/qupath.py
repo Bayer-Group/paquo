870150794644e5501b225cd8d50bb0b5470b2f40
Andreas Poehlmann <andreas@poehlmann.io>
2020-07-09 11:47:48 +0200

    paquo.qupath: streamline find_qupath
    for now we only support discovery of the conda installed qupath

--- paquo/qupath.py
+++ paquo/qupath.py
@@ -1,64 +1,55 @@
 import os
 import sys
 from pathlib import Path
-from typing import Tuple, Optional, List
+from typing import Tuple, List
 
 
-def conda_env_prefix() -> Optional[Path]:
+def find_qupath() -> Tuple[Path, Path, Path, List[str]]:
+    """find current qupath installation and jvm paths/options
+
+    For now this supports a qupath which ships its own JRE installation
+    and was installed via `conda install -c sdvillal qupath`
+
+    Returns
+    -------
+    qupath_jvm_info:
+        a tuple (app_dir, runtime_dir, jvm_path, jvm_options)
+    """
+    # test if we're running in conda
     prefix = os.environ.get('CONDA_PREFIX')
-    if prefix:
-        return Path(prefix)
-    return None
+    if not prefix:
+        # for now we require the conda install
+        raise NotImplementedError("paquo requires a conda installed qupath")
+    conda_prefix = Path(prefix)
 
-# N.B. we could depend on conda itself and depend on its API, but we will just use paths here
+    # hardcoded paths as in the conda recipe at:
+    # github.com/bayer-science-for-a-better-life/qupath-feedstock
+    if sys.platform == "linux" or sys.platform == "linux2":
+        app_dir = conda_prefix / 'opt' / 'QuPath' / 'lib' / 'app'
+        runtime_dir = conda_prefix / 'opt' / 'QuPath' / 'lib' / 'runtime'
+        jvm_path = runtime_dir / 'lib' / 'server' / 'libjvm.so'
+        jvm_options = []
 
+    elif sys.platform == "darwin":
+        qupath_dir = conda_prefix / 'bin' / 'QuPath.app'
+        app_dir = qupath_dir / 'Contents' / 'app'
+        runtime_dir = qupath_dir / 'Contents' / 'runtime' / 'Contents' / 'Home'
+        jvm_path = runtime_dir / 'lib' / 'server' / 'libjvm.dylib'
+        jvm_options = [
+            f'-Djava.library.path={app_dir}:{qupath_dir}/Contents/MacOS',
+            f'-Djava.launcher.path={qupath_dir}/Contents/MacOS',
+        ]
 
-def qupath_conda_paths() -> Optional[Tuple[Path, Path, Path, List[str]]]:
-    """
-    Finds qupath and relevant JRE installation paths in the current conda environment
+    elif sys.platform == "win32":
+        app_dir = conda_prefix / 'Library' / 'QuPath' / 'app'
+        runtime_dir = conda_prefix / 'Library' / 'QuPath' / 'runtime'
+        jvm_path = None  # TODO
+        jvm_options = []
 
-    Returns a tuple (app_dir, runtine_dir, jvm_path, jvm_options), or None if not found.
-    """
+    else:
+        raise ValueError(f'Unknown platform {sys.platform}')
 
-    conda_prefix = conda_env_prefix()
-    if conda_prefix:
-        if sys.platform == "linux" or sys.platform == "linux2":
-            app_dir = conda_prefix / 'opt' / 'QuPath' / 'lib' / 'app'
-            runtime_dir = conda_prefix / 'opt' / 'QuPath' / 'lib' / 'runtime'
-            jvm_path = runtime_dir / 'lib' / 'server' / 'libjvm.so'
-            jvm_options = []
-        elif sys.platform == "darwin":
-            qupath_dir = conda_prefix / 'bin' / 'QuPath.app'
-            app_dir = qupath_dir / 'Contents' / 'app'
-            runtime_dir = qupath_dir / 'Contents' / 'runtime' / 'Contents' / 'Home'
-            jvm_path = runtime_dir / 'lib' / 'server' / 'libjvm.dylib'
-            jvm_options = [
-                f'-Djava.library.path={app_dir}:{qupath_dir}/Contents/MacOS',
-                f'-Djava.launcher.path={qupath_dir}/Contents/MacOS',
-                # '-Djavafx.macosx.embedded=true',
-                # '-XstartOnFirstThread',
-                # '-Djava.awt.headless=true',
-                # '-XX:MaxRAMPercentage=50',
-            ]
-        elif sys.platform == "win32":
-            app_dir = conda_prefix / 'Library' / 'QuPath' / 'app'
-            runtime_dir = conda_prefix / 'Library' / 'QuPath' / 'runtime'
-            jvm_path = None  # TODO
-            jvm_options = []
-        else:
-            raise Exception('Unknown platform {}'.format(sys.platform))
-        if app_dir.is_dir() and runtime_dir.is_dir():
-            return app_dir, runtime_dir, jvm_path, jvm_options
-    return None
+    if not (app_dir.is_dir() and runtime_dir.is_dir() and jvm_path.is_file()):
+        raise FileNotFoundError('qupath installation is incompatible')
 
-
-def find_qupath() -> Optional[Tuple[Path, Path, Path, List[str]]]:
-    """
-    Finds current qupath and relevant JRE installation paths, returning a tuple (app_dir, runtine_dir).
-    """
-    # FIXME: merge this with qupath_conda_paths
-    app_dir, runtime_dir, jvm_path, jvm_options = qupath_conda_paths()
-    assert app_dir.is_dir()
-    assert runtime_dir.is_dir()
-    assert jvm_path.is_file()
     return app_dir, runtime_dir, jvm_path, jvm_options
