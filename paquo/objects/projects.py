a7c88cc57823fbdd01b25611f844dfc037c1ae43
Andreas Poehlmann <andreas@poehlmann.io>
2020-07-10 20:23:58 +0200

    paquo.objects.projects: implement adding images (wip)

--- paquo/objects/projects.py
+++ paquo/objects/projects.py
@@ -1,15 +1,91 @@
 import pathlib
+from collections.abc import MutableSet
 from typing import Union
+
 from paquo.qupath.jpype_backend import java_import, jvm_running
 
 # import java classes
 with jvm_running():
     _BufferedImage = java_import('java.awt.image.BufferedImage')
-    # _DefaultProject = java_import('qupath.lib.projects.DefaultProject')
+    _DefaultProject = java_import('qupath.lib.projects.DefaultProject')
     # _Project = java_import('qupath.lib.projects.Project')
     _Projects = java_import('qupath.lib.projects.Projects')
     _ProjectIO = java_import('qupath.lib.projects.ProjectIO')
     _File = java_import('java.io.File')
+    _String = java_import('java.lang.String')
+    _DefaultProjectImageEntry = java_import('qupath.lib.projects.DefaultProject.DefaultProjectImageEntry')
+    _ImageServerProvider = java_import('qupath.lib.images.servers.ImageServerProvider')
+
+
+class QuPathProjectImageEntry:
+
+    def __init__(self, entry):
+        if not isinstance(entry, _DefaultProjectImageEntry):
+            raise TypeError("don't instantiate QuPathProjectImageEntry yourself")
+        self._entry = entry
+
+    @property
+    def id(self):
+        return str(self._entry.getID())
+
+    @property
+    def image_name(self):
+        return str(self._entry.getImageName())
+
+    @image_name.setter
+    def image_name(self, name):
+        self._entry.setImageName(_String(name))
+
+    @property
+    def image_name_original(self):
+        org_name = self._entry.getOriginalImageName()
+        return str(org_name) if org_name else None
+
+    @property
+    def entry_path(self):
+        return pathlib.Path(self._entry.getEntryPath().toString())
+
+    @property
+    def thumbnail(self):
+        return self._entry.getThumbnail()
+
+
+class _QuPathProjectImageEntriesProxy(MutableSet):
+
+    def discard(self, x: QuPathProjectImageEntry) -> None:
+        pass
+
+    def add(self, filename) -> None:
+        # first get a server builder
+        img_path = pathlib.Path(filename).absolute()
+        support = _ImageServerProvider.getPreferredUriImageSupport(_BufferedImage, _String(str(img_path)))
+        server_builders = list(support.getBuilders())
+        if not server_builders:
+            raise Exception("unsupported file")
+        server_builder = server_builders[0]
+        self._project.addImage(server_builder)
+
+    def __init__(self, project):
+        if not isinstance(project, _DefaultProject):
+            raise TypeError('requires _DefaultProject instance')
+        self._project = project
+        self._it = iter(())
+
+    def __len__(self):
+        return self._project.size()
+
+    def __contains__(self, __x: object) -> bool:
+        if not isinstance(__x, _DefaultProjectImageEntry):
+            return False
+        return False  # FIXME
+
+    def __iter__(self):
+        self._it = iter(self._project.getImageList())
+        return self
+
+    def __next__(self):
+        image_entry = next(self._it)
+        return QuPathProjectImageEntry(image_entry)
 
 
 class QuPathProject:
@@ -21,10 +97,21 @@ class QuPathProject:
         else:
             self._project = _Projects.createProject(_File(str(path)), _BufferedImage)
 
-    # TODO: implement list/set interface
-    #   image entries can be duplicated. but uri should be unique.
-    # some list like proxy
-    # images = []
+        self._image_entries_proxy = _QuPathProjectImageEntriesProxy(self._project)
+
+    @property
+    def images(self):
+        return self._image_entries_proxy
+
+    @images.setter
+    def images(self, images):
+        _images = []
+        for image in images:
+            if not isinstance(image, QuPathProjectImageEntry):
+                raise TypeError("images needs to be iterable of instances of QuPathProjectImageEntry")
+        self.images.clear()
+        for image in _images:
+            self.images.add(image)
 
     def __len__(self):
         # return len(self.images)
@@ -76,7 +163,7 @@ class QuPathProject:
         return self._project.getName()
 
     def __repr__(self):
-        name = self._project.getNameFromURI(self.uri)
+        name = self._project.getNameFromURI(self._project.getURI())
         return f'<QuPathProject {name}>'
 
     @property
