f5f4b684fe77c383e4e55d7b241832e97ee79504
Andreas Poehlmann <andreas@poehlmann.io>
2020-07-10 08:52:38 +0200

    paquo.qupath.jpype_backend: cleanup and add java_import
    move to importing the required java classes in the submodules
    that need it.

--- paquo/qupath/jpype_backend.py
+++ paquo/qupath/jpype_backend.py
@@ -1,24 +1,20 @@
-from pathlib import Path
+from contextlib import contextmanager, nullcontext
 import jpype
 
 from paquo.qupath.util import find_qupath
 
 
-def start_jpype():
+@contextmanager
+def jvm_running():
     if not jpype.isJVMStarted():
         # For the time being, we assume qupath is our JVM of choice
         app_dir, runtime_dir, jvm_path, jvm_options = find_qupath()
         # This is not really needed, but beware we might need SL4J classes (see warning)
         jpype.addClassPath(str(app_dir / '*'))
-        # Our custom java libraries
-        JAVA_VENDORED_PATH = Path(__file__).absolute().parent / 'vendored' / 'java' / 'jars'
-        if JAVA_VENDORED_PATH.is_dir():
-            jpype.addClassPath(str(JAVA_VENDORED_PATH / '*'))
         jpype.startJVM(str(jvm_path), convertStrings=False)
+    yield
 
 
-if __name__ == '__main__':
-    start_jpype()
-    BuildInfo = jpype.JClass('qupath.lib.gui.BuildInfo')
-    # now this can be made easy with qupath
-    print(BuildInfo.getInstance().getVersion())
+def java_import(name, cm=nullcontext):
+    with cm():
+        return jpype.JClass(name)
