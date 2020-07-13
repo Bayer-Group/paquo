b17f811a44c03275fc5989d79f5d12c5a5246e19
Andreas Poehlmann <andreas@poehlmann.io>
2020-07-13 18:38:16 +0200

    paquo.objects.hierarchy: implement poc serialization for polygons
    This needs a lot of work still...
    The actual interface needs to reworked, and we need to figure
    out what we want to expose. Also all other annotationtypes need
    to be wrapped and implemented.
    
    But inserting polygonal annotations into qupath projects generated
    from python works. :tada:

--- paquo/objects/hierarchy.py
+++ paquo/objects/hierarchy.py
@@ -1,13 +1,17 @@
 import json
-
-from paquo.qupath.jpype_backend import jvm_running, java_import
+from typing import List
 
 from paquo.objects.classes import PathClass
+from paquo.qupath.jpype_backend import jvm_running, java_import
 
 with jvm_running():
     _String = java_import('java.lang.String')
     _ColorTools = java_import('qupath.lib.common.ColorTools')
     _GsonTools = java_import('qupath.lib.io.GsonTools')
+    _Point2 = java_import("qupath.lib.geom.Point2")
+    _ROIs = java_import("qupath.lib.roi.ROIs")
+    _PathObjects = java_import("qupath.lib.objects.PathObjects")
+    _ArrayList = java_import("java.util.ArrayList")
 
 
 class PathObjectHierarchy:
@@ -24,12 +28,60 @@ class PathObjectHierarchy:
 
     @property
     def root(self):
-        return PathObject(self._hierarchy.getRootObject())
+        root = self._hierarchy.getRootObject()
+        if root is None:
+            return None
+        return PathObject(root)
 
     def to_geojson(self):
         gson = _GsonTools.getInstance()
         geojson = gson.toJson(self._hierarchy.getAnnotationObjects())
         return json.loads(str(geojson))
+
+    def from_geojson(self, geojson):
+        # fixme: implement nicely...
+        """
+        def annotations = []
+        for (annotation in tumorAnnotations) {
+            def name = annotation['name']
+            def vertices = annotation['vertices']
+            def points = vertices.collect {new Point2(it[0], it[1])}
+            def polygon = new PolygonROI(points)
+            def pathAnnotation = new PathAnnotationObject(polygon)
+            pathAnnotation.setName(name)
+            annotations << pathAnnotation
+        }
+
+        // Add to current hierarchy
+        QPEx.addObjects(annotations)
+
+        [{
+            'type': 'Feature',
+            'id': 'PathAnnotationObject',
+            'geometry': {
+                'type': 'Polygon',
+                'coordinates': [[
+                    [1058, 1379],
+                    [1054, 1380],
+                    [1058, 1379]
+                ]]
+            },
+            'properties': {
+                'classification': {
+                    'name': 'Tumor',
+                    'colorRGB': -3670016
+                },
+                'isLocked': False,
+                'measurements': []
+            }
+        }]
+        """
+        assert isinstance(geojson, list)
+        changed = False
+        for annotation in geojson:
+            ao = PathAnnotationObject.from_geojson(annotation)
+            changed |= self._hierarchy.insertPathObject(ao._path_object, True)
+        return changed
 
 
 class PathObject:
@@ -111,3 +163,27 @@ class PathObject:
     # ... todo: needs more pythonic interface
     # def set_path_class(self, pc: PathClass, probability: float = float("nan")):
     #     self._path_object.setPathClass(pc._path_class, probability)
+
+
+class PathAnnotationObject(PathObject):
+
+    @classmethod
+    def from_vertices(cls, vertices: List[List[List[float]]], path_class: PathClass = None):
+        # fixme: quick and dirty poc
+        assert len(vertices) == 1
+        points = _ArrayList()
+        for x, y in vertices[0]:
+            points.add(_Point2(x, y))
+        roi = _ROIs.createPolygonROI(points, None)
+        ao = _PathObjects.createAnnotationObject(roi, path_class._path_class, None)
+        return cls(ao)
+
+    @classmethod
+    def from_geojson(cls, geojson):
+        assert geojson['geometry']['type'] == 'Polygon'
+        path_class = PathClass.create(
+            name=geojson['properties']['classification']['name'],
+            color=(0, 255, 0)
+        )
+        polygon = geojson['geometry']['coordinates']
+        return cls.from_vertices(polygon, path_class)
