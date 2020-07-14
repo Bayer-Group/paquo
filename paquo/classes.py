74d4e7aa89843e1d56bcec26797e965a1a4ee9ad
Andreas Poehlmann <andreas@poehlmann.io>
2020-07-14 18:54:32 +0200

    paquo.classes: PathClass abstraction

--- paquo/classes.py
+++ paquo/classes.py
@@ -1,100 +1,128 @@
+from __future__ import annotations
+from typing import Optional
+
+from paquo._base import QuPathBase
+from paquo.colors import QuPathColor, ColorType
 from paquo.java import PathClass, PathClassFactory, ColorTools
 
 
-class QuPathPathClass:
+class QuPathPathClass(QuPathBase[PathClass]):
 
-    def __init__(self, path_class=None):
+    def __init__(self, path_class: PathClass) -> None:
+        """initialize a QuPathPathClass from its corresponding java PathClass"""
         if not isinstance(path_class, PathClass):
             raise ValueError("use PathClass.create() to instantiate")
-        self._path_class = path_class
+        super().__init__(path_class)
 
     @classmethod
-    def create(cls, name, color=None, parent=None, exist_ok=True):
-        if name is None and parent is not None:
-            raise ValueError("cannot create derived PathClass with name=None")
+    def create(cls,
+               name: str,
+               color: Optional[ColorType] = None,
+               parent: Optional[QuPathPathClass] = None) -> QuPathPathClass:
+        """create a QuPathPathClass
 
-        _parent_class = None
+        The QuPathPathClasses are wrappers around singletons defined by their
+        name and their ancestors. Internally there's only one java PathClass
+        instance per ancestor chain + name. (The internal unique id is chaining
+        names via ':', which is why ':' is an unsupported name character)
+
+        Parameters
+        ----------
+        name:
+            the name of your path class
+        color:
+            a color (r,g,b) or (r,g,b,a) with 0 < x < 255 or a QuPathColor
+        parent:
+            the parent of the class
+
+        Returns
+        -------
+        path_class:
+            the QuPathPathClass
+        """
+        if name is None:
+            # note:
+            #   parent=None for name=None creates the root node on the qupath java side
+            #   will have to consider if we expose this here.
+            if parent is not None:
+                raise ValueError("cannot create derived QuPathPathClass with name=None")
+
+        # get the parent class if requested
+        java_parent = None
         if parent is not None:
-            if not isinstance(parent, PathClass):
-                raise TypeError("parent must be a PathClass")
-            _parent_class = parent._path_class
+            if not isinstance(parent, QuPathPathClass):
+                raise TypeError("parent must be a QuPathPathClass")
+            java_parent = parent.java_object
 
-        if not exist_ok:
-            # FIXME: derivedClassToString is not public...
-            path_class_str = PathClass.derivedClassToString(_parent_class, name)
-            if bool(PathClassFactory.classExists(path_class_str)):
-                raise Exception("path_class already created")
+        # set the color if requested
+        java_color = None
+        if color is not None:
+            java_color = QuPathColor.from_any(color).to_java_rgb()  # use rgba?
 
-        _color = ColorTools.makeRGB(int(color[0]), int(color[1]), int(color[2]))
-        path_class = PathClassFactory.getDerivedPathClass(_parent_class, name, _color)
+        path_class = PathClassFactory.getDerivedPathClass(java_parent, name, java_color)
         return cls(path_class)
 
     @property
-    def name(self):
-        return str(self._path_class.getName())
+    def name(self) -> str:
+        """the name of the class"""
+        return str(self.java_object.getName())
 
     @property
-    def id(self):
-        return str(self._path_class.toString())
+    def id(self) -> str:
+        """the unique identifier string of the class"""
+        return str(self.java_object.toString())
 
-    def __eq__(self, other):
-        return self._path_class.compareTo(other._path_class)
+    def __eq__(self, other: QuPathPathClass) -> bool:
+        # check if path classes are identical
+        if not isinstance(other, QuPathPathClass):
+            return False
+        return self.java_object.compareTo(other.java_object)
 
     @property
-    def parent(self):
-        path_class = self._path_class.getParentClass()
+    def parent(self) -> Optional[QuPathPathClass]:
+        """the parent path class of this path class"""
+        path_class = self.java_object.getParentClass()
         if path_class is None:
             return None
-        return PathClass(path_class)
+        return QuPathPathClass(path_class)
 
     @property
-    def origin(self):
+    def origin(self) -> QuPathPathClass:
+        """the toplevel parent of this path class"""
         origin_class = self
         while origin_class.parent is not None:
             origin_class = origin_class.parent
         return origin_class
 
-    def is_derived_from(self, parent_class):
-        return self._path_class.isDerivedFrom(parent_class._path_class)
+    def is_derived_from(self, parent_class: QuPathPathClass):
+        """is this class derived from the parent_class"""
+        return self.java_object.isDerivedFrom(parent_class.java_object)
 
-    def is_ancestor_of(self, child_class):
-        return self._path_class.isAncestorOf(child_class._path_class)
+    def is_ancestor_of(self, child_class: QuPathPathClass):
+        """is this class an ancestor of the child_class"""
+        return self.java_object.isAncestorOf(child_class.java_object)
 
     @property
-    def color(self):
-        argb = self._path_class.getColor()
-        r = int(ColorTools.red(argb))
-        g = int(ColorTools.green(argb))
-        b = int(ColorTools.blue(argb))
-        return r, g, b
+    def color(self) -> QuPathColor:
+        """return the path color"""
+        argb = self.java_object.getColor()
+        return QuPathColor.from_java_rgba(argb)
 
     @color.setter
-    def color(self, rgb):
-        r, g, b = map(int, rgb)
-        a = int(255 * self.alpha)
-        argb = ColorTools.makeRGBA(r, g, b, a)
-        self._path_class.setColor(argb)
+    def color(self, rgb: ColorType) -> None:
+        """set the path color"""
+        argb = ColorTools.from_any(rgb).to_java_argb()  # maybe use argb?
+        self.java_object.setColor(argb)
 
     @property
-    def alpha(self):
-        argb = self._path_class.getColor()
-        a = int(ColorTools.alpha(argb))
-        return a / 255.0
-
-    @alpha.setter
-    def alpha(self, alpha):
-        r, g, b = self.color
-        a = int(255 * alpha)
-        argb = ColorTools.makeRGBA(r, g, b, a)
-        self._path_class.setColor(argb)
+    def is_valid(self) -> bool:
+        """check if the path class is valid"""
+        return bool(self.java_object.isValid())
 
     @property
-    def is_valid(self):
-        return bool(self._path_class.isValid())
+    def is_derived_class(self) -> bool:
+        """check if the class is a derived path class"""
+        return bool(self.java_object.isDerivedClass())
 
-    @property
-    def is_derived_class(self):
-        return bool(self._path_class.isDerivedClass())
-
-    def __repr__(self):
-        return f"<PathClass '{self.id}'>"
+    def __repr__(self) -> str:
+        return f"<QuPathPathClass '{self.id}'>"
