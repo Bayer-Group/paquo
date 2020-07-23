29da65218fa1b2bb85a723bad5aa1fa010a27b2f
Andreas Poehlmann <andreas@poehlmann.io>
2020-07-23 23:09:33 +0200

    paquo.images: expose properties as dict-like mapping

--- paquo/tests/test_images.py
+++ paquo/tests/test_images.py
@@ -53,6 +53,20 @@ def test_metadata_non_str_keys(image_entry):
         image_entry.metadata["1"] = 123
 
 
+def test_properties_interface(image_entry):
+    from paquo.java import DefaultProject
+
+    assert len(image_entry.properties) == 1
+    assert str(DefaultProject.IMAGE_ID) in image_entry.properties
+
+    with pytest.raises(ValueError):
+        image_entry.properties[1] = 123
+
+    image_entry.properties["annotated"] = "no"
+
+    assert image_entry.properties["annotated"] == "no"
+
+
 def test_description(image_entry):
     assert image_entry.description == ""
     image_entry.description = "abc"
