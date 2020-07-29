9ffbcb09f90f864f92c2c1eef810954df6b1b307
Andreas Poehlmann <andreas@poehlmann.io>
2020-07-29 17:08:09 +0200

    tests: cover more paquo.classes code

--- paquo/tests/test_classes.py
+++ paquo/tests/test_classes.py
@@ -9,9 +9,39 @@ def pathclass():
 
 
 def test_pathclass_creation():
+    with pytest.raises(TypeError):
+        QuPathPathClass("abc")
+
     pc = QuPathPathClass.create("MyClass", color=None)
     assert pc.parent is None
     assert pc.name == pc.id == "MyClass"
+    assert pc.is_valid
+    assert "MyClass" in repr(pc)
+
+
+def test_deny_name_none_creation():
+    with pytest.raises(NotImplementedError):
+        # noinspection PyTypeChecker
+        QuPathPathClass.create(None, parent=None)
+
+    pc = QuPathPathClass.create("MyClass")
+    with pytest.raises(ValueError):
+        # noinspection PyTypeChecker
+        QuPathPathClass.create(None, parent=pc)
+
+
+def test_incorrect_parent_type():
+    with pytest.raises(TypeError):
+        # noinspection PyTypeChecker
+        QuPathPathClass.create("new class", parent="parent_class")
+
+
+def test_incorrect_class_name():
+    with pytest.raises(TypeError):
+        # noinspection PyTypeChecker
+        QuPathPathClass.create(1)
+    with pytest.raises(ValueError):
+        QuPathPathClass.create("my::class")
 
 
 def test_pathclass_equality(pathclass):
@@ -20,6 +50,7 @@ def test_pathclass_equality(pathclass):
     assert pathclass == pathclass
     assert pathclass != other
     assert pathclass == same
+    assert pathclass != 123
 
 
 def test_pathclass_creation_with_parent(pathclass):
@@ -27,6 +58,14 @@ def test_pathclass_creation_with_parent(pathclass):
     assert pc.parent == pathclass
     assert pc.name == "MyChild"
     assert pc.id == "MyClass: MyChild"
+
+    assert pc.origin == pathclass
+    assert pc.is_derived_from(pathclass)
+    assert not pc.is_ancestor_of(pathclass)
+    assert not pathclass.is_derived_from(pc)
+    assert pathclass.is_ancestor_of(pc)
+    assert pc.is_derived_class
+    assert not pathclass.is_derived_class
 
 
 def test_pathclass_colors():
@@ -36,3 +75,6 @@ def test_pathclass_colors():
 
     pc = QuPathPathClass.create("MyNew2", color=(1, 2, 3))
     assert pc.color.to_rgb() == (1, 2, 3)
+
+    pc.color = "#ff0000"
+    assert pc.color.to_rgb() == (255, 0, 0)
