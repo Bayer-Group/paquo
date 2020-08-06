0d757cb4b1dc45e7a101a192870d42cf531eac2e
Andreas Poehlmann <andreas@poehlmann.io>
2020-08-06 20:53:26 +0200

    paquo._utils: implement backup function

--- paquo/_utils.py
+++ paquo/_utils.py
@@ -1,3 +1,7 @@
+from functools import wraps
+from io import UnsupportedOperation
+from pathlib import Path
+from typing import Optional
 
 try:
     from functools import cached_property  # type: ignore
@@ -19,4 +23,19 @@ try:
 except ImportError:
     from contextlib import suppress as nullcontext  # works with 3.4+
 
-__all__ = ['cached_property', 'nullcontext']
+
+def stash_project_files(project_dir: Path):
+    """move rename projects files in a project to .backup"""
+    if not project_dir.is_dir():
+        return
+    for old_file in project_dir.iterdir():
+        if old_file.suffix == ".backup":
+            continue
+        new_file = old_file.with_suffix(f"{old_file.suffix}.backup")
+        i = 0
+        while new_file.is_file():
+            new_file = old_file.with_suffix(f"{old_file.suffix}.{i}.backup")
+        old_file.rename(new_file)
+
+
+__all__ = ['cached_property', 'nullcontext', 'stash_project_files']
