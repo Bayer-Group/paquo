abb1e6c87f75ec5daebdf9ca223bc29ea868016e
Andreas Poehlmann <andreas@poehlmann.io>
2020-08-12 16:29:00 +0200

    tests: add tests for export command

--- paquo/tests/test_main.py
+++ paquo/tests/test_main.py
@@ -44,3 +44,28 @@ def test_config_cmd(tmp_path):
     assert run(main, ['config', '-l', '-o', str(tmp_path), '--force']).return_code == 0
     # allow showing all folders where you can store you paquo config
     assert run(main, ['config', '--search-tree']).return_code == 0
+
+
+def test_export_cmd(tmpdir, tmp_path, svs_small):
+    from shapely.geometry import Point
+    from paquo.projects import QuPathProject
+    from paquo.images import QuPathImageType
+
+    with QuPathProject(tmp_path) as qp:
+        entry = qp.add_image(svs_small, image_type=QuPathImageType.BRIGHTFIELD_OTHER)
+        entry.hierarchy.add_annotation(
+            roi=Point(500, 500)
+        )
+
+    # help
+    assert run(main, ['export']).return_code == 0
+    # wrong index
+    assert run(main, ['export', str(tmp_path), '-i', '26']).return_code == 1
+    # get index
+    assert run(main, ['export', str(tmp_path), '-i', '0']).return_code == 0
+    # check pretty print
+    assert run(main, ['export', str(tmp_path), '-i', '0', '--pretty']).return_code == 0
+    # check file output
+    with tmpdir.as_cwd():
+        assert run(main, ['export', str(tmp_path), '-i', '0', '--pretty', '-o', 'data.geojson']).return_code == 0
+        assert (Path(tmpdir) / "data.geojson").is_file()
